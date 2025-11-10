54162a34ab696c5493293560e4652dba66f73de0
DomiCarr <carrasco.dominique@gmail.com>
2025-11-10 21:28:21 +0100

    :construction: work in progress : code tunning & cleaning

--- concat.sh
+++ concat.sh
@@ -1,7 +1,14 @@
 #!/bin/zsh
 
+# Output file
+output_file="code.txt"
+
 # Nettoyage de la sortie si elle existe
 rm -f code.txt
+
+# Add generated date and time at the top
+echo "Generated $(date '+%Y-%m-%d %H:%M:%S')" >> "$output_file"
+echo "\n\n" >> "$output_file"
 
 # Ajout du HTML
 echo "===== index.html =====" >> code.txt
@@ -13,6 +20,15 @@ for f in style/*.css; do
   echo "===== ${f} =====" >> code.txt
   cat "$f" >> code.txt
   echo "\n\n" >> code.txt
+done
+
+# Find all .js files recursively, sort alphabetically, handle spaces
+find . -type f -name "*.js" | sort | while IFS= read -r f; do
+echo "===========================================================================================" >> "$output_file"
+  echo "    $f " >> "$output_file"
+echo "===========================================================================================" >> "$output_file"
+  cat "$f" >> "$output_file"
+  echo "\n\n" >> "$output_file"
 done
 
 echo " Concaténation terminée → code.txt généré"
